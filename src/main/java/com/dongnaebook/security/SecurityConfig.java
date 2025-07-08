9492a8b87292ea5a70a53b7440800d7cfde13cef
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-07-08 10:36:52 +0900

    logout

--- src/main/java/com/dongnaebook/security/SecurityConfig.java
+++ src/main/java/com/dongnaebook/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dongnaebook.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,7 +9,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -16,11 +20,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // API 서버라면 csrf 보통 꺼둡니다.
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/signup", "/api/users/login").permitAll() // 회원가입, 로그인은 인증 없이 허용!
-                        .anyRequest().authenticated() // 나머지는 인증 필요
-                );
+                        .requestMatchers("/api/users/signup", "/api/users/login").permitAll() // 회원가입, 로그인은 인증 없이해야함
+                        .anyRequest().authenticated()
+                )
+         .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
 }
